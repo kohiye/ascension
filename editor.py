b69842ff15bec5363adc49a77799e02601240978
ivan <procciy@proton.me>
2023-11-11 15:55:02 +0000

    added a way to add tiles in editor

--- editor.py
+++ editor.py
@@ -21,10 +21,15 @@ class Editor:
         self.guide_surf = pygame.Surface((s.WINDOW_WIDTH, s.WINDOW_WIDTH))
         self.guide_surf.set_colorkey("green")
 
+        # canvas stuff:
+        self.canvas_data = {}
+        self.last_cell = None
+
     def event_loop(self):
         for event in pygame.event.get():
             self.mode_switch(event)
             self.pan_movement(event)
+            self.canvas_add()
 
     def mode_switch(self, event):
         if event.type == pygame.QUIT:
@@ -32,6 +37,10 @@ class Editor:
         if event.type == pygame.KEYDOWN:
             if event.key == pygame.K_ESCAPE:
                 self.mode = 0
+
+    def get_current_cell(self):
+        temp_vect = (vector(mouse_pos()) - self.origin) // s.TILE_SIZE
+        return (temp_vect.x, temp_vect.y)
 
     def pan_movement(self, event):
         if event.type == pygame.MOUSEBUTTONDOWN and mouse_buttons()[1]:
@@ -41,6 +50,18 @@ class Editor:
             self.pan_mode = False
         if self.pan_mode:
             self.origin = vector(mouse_pos()) - self.pan_offset
+
+    def canvas_add(self):
+        if mouse_buttons()[2]:
+            current_cell = self.get_current_cell()
+
+            if current_cell != self.last_cell:
+                if current_cell in self.canvas_data:
+                    pass
+                else:
+                    self.canvas_data[current_cell] = "occupied"
+                self.last_cell = current_cell
+                print(self.canvas_data)
 
     def draw_tile_guides(self):
         cols = s.WINDOW_WIDTH // s.TILE_SIZE
