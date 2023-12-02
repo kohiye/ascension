5efbcb0473f87f7978ec0e946deeb0b73eb03a84
ivan <procciy@proton.me>
2023-12-02 19:52:20 +0000

    enemy movement

--- code/lvlsprites.py
+++ code/lvlsprites.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.math import Vector2 as vector
 
 import settings as s
+from support import signum
 
 
 class Generic(pygame.sprite.Sprite):
@@ -112,9 +113,114 @@ class Player(Generic):
 
 
 class Enemy(Generic):
-    def __init__(self, pos, group):
+    def __init__(self, pos, group, player, collision_sprites):
         super().__init__(pos, pygame.Surface((80, 80)), group)
-        self.image.fill("red")
+        self.image.fill("blue")
+        self.speed = vector()
+        self.thrust = vector()
+        self.drag = vector()
+        self.shift = vector(self.rect.topleft)
+        self.drag_coeff = 0.01
+
+        self.player = player
+        self.collision_sprites = collision_sprites
+        self.last_target = None
+        self.target = vector(self.rect.center)
+
+        self.hitbox = self.rect.inflate(-40, -40)
+        self.repulsion_rect = self.rect.inflate(40, 40)
+        self.repulsion = vector()
+
+    def input(self):
+        target_diff = vector(pygame.mouse.get_pos()) - vector(self.rect.center)
+        if target_diff != vector((0, 0)):
+            self.thrust = 500 * target_diff.normalize()
+
+    def enemy_vision(self):
+        obstuctions = []
+        for sprite in self.collision_sprites:
+            if sprite.rect.clipline(self.player.rect.center, self.rect.center):
+                obstuctions.append(sprite)
+
+        if obstuctions:
+            self.terget = self.last_target
+            self.image.fill("blue")
+        else:
+            self.target = vector(self.player.rect.center)
+            self.last_target = self.target
+            self.image.fill("red")
+
+    def friction(self):
+        self.drag.x = (
+            self.drag_coeff * (self.speed.x**2)
+            if self.speed.x < 0
+            else -self.drag_coeff * (self.speed.x**2)
+        )
+        self.drag.y = (
+            self.drag_coeff * (self.speed.y**2)
+            if self.speed.y < 0
+            else -self.drag_coeff * (self.speed.y**2)
+        )
+
+    def move(self, dt):
+        self.friction()
+        self.repulsion_check()
+
+        self.speed.x += (self.thrust.x + self.drag.x + 200 * self.repulsion.x) * dt
+        self.speed.y += (self.thrust.y + self.drag.y + 200 * self.repulsion.y) * dt
+
+        self.shift.x += self.speed.x * dt
+        self.hitbox.x = round(self.shift.x)
+        self.repulsion_rect.centerx = self.hitbox.centerx
+        self.collistion_check("X")
+        self.rect.centerx = self.hitbox.centerx
+
+        self.shift.y += self.speed.y * dt
+        self.hitbox.y = round(self.shift.y)
+        self.repulsion_rect.centery = self.hitbox.centery
+        self.collistion_check("Y")
+        self.rect.centery = self.hitbox.centery
+
+    def repulsion_check(self):
+        self.repulsion = vector()
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.repulsion_rect):
+                repulsion_distance = vector(self.rect.center) - vector(
+                    sprite.rect.center
+                )
+                repulsion_multi = repulsion_distance.normalize() * (
+                    100 / repulsion_distance.magnitude()
+                )
+                self.repulsion += repulsion_multi
+
+        if abs(self.repulsion.x) < 2:
+            self.repulsion.x = 0
+        if abs(self.repulsion.y) < 2:
+            self.repulsion.y = 0
+
+    def collistion_check(self, direction):
+        for sprite in self.collision_sprites:
+            if sprite.rect.colliderect(self.hitbox):
+                if direction == "X":
+                    if self.speed.x > 0:
+                        self.hitbox.right = sprite.rect.left
+                    if self.speed.x < 0:
+                        self.hitbox.left = sprite.rect.right
+                    self.shift.x = self.hitbox.x
+                    self.speed.x = -self.speed.x
+
+                if direction == "Y":
+                    if self.speed.y > 0:
+                        self.hitbox.bottom = sprite.rect.top
+                    if self.speed.y < 0:
+                        self.hitbox.top = sprite.rect.bottom
+                    self.shift.y = self.hitbox.y
+                    self.speed.y = -self.speed.y
+
+    def update(self, dt):
+        self.enemy_vision()
+        self.input()
+        self.move(dt)
 
 
 class Prop(Generic):
